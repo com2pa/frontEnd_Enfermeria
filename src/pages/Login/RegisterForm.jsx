1272002ba03e8b1702c926f480b79e3718de1359
merwil vegas <com2pa@gmail.com>
2024-06-23 22:51:02 -0400

    se añidio validaciones para el formulario de registro de usuario

--- src/pages/Login/RegisterForm.jsx
+++ src/pages/Login/RegisterForm.jsx
@@ -1,5 +1,5 @@
-import { Button, ButtonGroup, FormControl, FormLabel, Heading, useToast} from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react'
+import { Box, Button, ButtonGroup, Flex, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, useToast} from '@chakra-ui/react';
+import { useEffect, useState } from 'react'
 import FormContainer from './From';
 import { baseUrl } from '../../config/url';
 import axios from 'axios';
@@ -42,14 +42,14 @@ export const RegisterForm = ({handleShow}) => {
 
   const handleNewUser = async ()=>{
     try {
-      const{data} = await axios.post(`${baseUrl}/api/users`,{name:name});
+      const{data} = await axios.post(`${baseUrl}/api/users`);
       toast({
         position:'top',
         title: 'Success',
         description: data.message,
-        status:'success',
-        duration: 9000,
-        isClosable: true,
+        status:'Correcto',
+        // duration: 9000,
+        // isClosable: true,
       });
       handleShow();
     } catch (error) {
@@ -57,9 +57,9 @@ export const RegisterForm = ({handleShow}) => {
         position:'top',
         title: 'Error',
         description: error.response.data.error,
-        status: 'error',
-        duration: 9000,
-        isClosable: true,
+        status: 'Hubo un error verique sus datos !',
+        // duration: 9000,
+        // isClosable: true,
       })
       console.log(error);
     }
@@ -87,21 +87,91 @@ export const RegisterForm = ({handleShow}) => {
    
     <FormContainer>
       <Heading>Register</Heading>
-      <FormControl>
-        <FormLabel>Nombre</FormLabel>
-        <input onChange={handleNameInput} type="text" placeholder="Nombre" /> 
-        <FormLabel>Email</FormLabel>   
-        <input onChange={handleEmailInput} type="email" placeholder="Correo" />
-        <FormLabel>Telefono</FormLabel>
-        <input onChange={handlePhoneInput} type="text" placeholder="Telefono" />
-        <FormLabel>Contraseña</FormLabel>
-        <input onChange={handlePasswordInput} type="password" placeholder="Contraseña" />
+      <FormControl w="100%">
+        
+          <Flex marginBottom="1rem">
+            <FormLabel>Nombre</FormLabel>
+            <input onChange={handleNameInput} type="text" placeholder="Nombre Apellido" value={name}  />
+          </Flex> 
+
+        
+          {nameValidation  ? ''
+            
+          : ( 
+            <FormHelperText color='red' border='0.5px solid red' margin='0.5rem' padding='1rem'>
+                <p> Debe comenzar con mayuscula tanto el nombre como apellido</p>
+                <p>1.- Primer caracter en Mayuscula del nombre seguido el resto del nombre en minuscula</p>
+                <p>2.- Primer caracter en Mayuscula deL apellido seguido el resto del nombre en minuscula
+                Merwil Vegas</p>
+            </FormHelperText>
+            // <FormErrorMessage>
+            //     Tu nombre no es valido
+            // </FormErrorMessage>
+          )}
+
+        <Flex marginBottom="1rem">
+          <FormLabel>Email</FormLabel>   
+          <input onChange={handleEmailInput}  type="email" placeholder="Correo"  value={email} />
+        </Flex> 
+        {emailValidation ?''
+         
+        : (
+          <FormHelperText color='red' border='0.5px solid red' margin='0.5rem' padding='1rem'>
+            Debe ser un correo valido
+          </FormHelperText>
+          // <FormErrorMessage>
+          //   Tu correo no es valido
+          // </FormErrorMessage>
+        )
+        }
+        <Flex marginBottom="1rem">
+          <FormLabel>Telefono</FormLabel>
+          <input onChange={handlePhoneInput} type="text" placeholder="Telefono" value={phone}  />
+        </Flex>
+        {phoneValidation ?''
+          
+        :(
+          <FormHelperText  color='red' border='0.5px solid red' margin='0.5rem' padding='1rem'>
+            Debe ser un numero de telefono valido
+            <p>1-. Debe comenzar con 0</p>
+            <p>2.- seguido 212  o 412 o 414 o 416 424  o 426</p>
+          </FormHelperText>
+          // <FormErrorMessage>
+          //   Tu numero de telefono no es valido
+          // </FormErrorMessage>
+        )
+
+        }
+        <Flex marginBottom="1rem">
+          <FormLabel>Contraseña</FormLabel>
+          <input onChange={handlePasswordInput}  type="password" placeholder="Contraseña"  />
+        </Flex>
+        { passwordValidation ? ''
+
+          
+
+          :(
+            <FormHelperText color='red' border='0.5px solid red' margin='0.5rem' padding='1rem'>
+            <p>Debe contener al menos una mayuscula, una minuscula, un numero y un caracter especial </p> 
+            <p>1.- Contiene al menos una letra mayúscula ([A-Z]). </p>
+            <p>2.- Contiene al menos una letra minúscula ([a-z]). </p>
+            <p>3.- Contiene al menos un dígito . </p>
+            <p>4.- No tiene espacios en blanco. </p>
+            <p>5.- Contiene al menos un carácter especial que no sea letra ni dígito . </p>
+            <p>6.- Tiene una longitud total entre 8 y 15 caracteres. </p>
+          </FormHelperText>
+            // <FormErrorMessage>
+            //   Tu contraseña no es valida
+            // </FormErrorMessage>
+
+          )
+        }
         {/* <input type="password" placeholder="Confirmar contraseña" /> */}
         
       </FormControl>
       <ButtonGroup>
-        <Button bg="green" onClick={handleNewUser}>Register</Button>
         <Button onClick={handleShow} variant="ghost"> Ingresar</Button>
+        <Button colorScheme="green" onClick={handleNewUser}>Register</Button>
       </ButtonGroup>
 
     </FormContainer>
