9d340dcdaffa18e0286b5e054909d79da26fa30e
merwil vegas <com2pa@gmail.com>
2024-07-19 19:29:06 -0400

    se modifico para  tener por separado rutas privada y publicas y su atenticacion de usuaio

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -1,23 +1,22 @@
 
-import { BrowserRouter, } from 'react-router-dom';
-import axios from 'axios';
+import { BrowserRouter, Route, Routes, } from 'react-router-dom';
+// import axios from 'axios';
 import Public from '../router/Public';
 import PrivateRoute from '../router/PrivateRoute'
 import { useAuth } from '../hooks/useAuth';
-import PersistAuth from '../components/PersistAuth';
+// import PersistAuth from '../components/PersistAuth';
 
 
 
 
 export const Root = () => {
-
+  const { isAuthenticated } = useAuth();
   return (
 
 
-    <BrowserRouter>
-      
-      <PrivateRoute />
-      <Public />
+    <BrowserRouter>  
+        <PrivateRoute />
+        <Public />
     </BrowserRouter>
 
 
