4664fbf977b456e0dc2beef4e486dd1104d8f965
merwil vegas <com2pa@gmail.com>
2024-07-19 19:40:44 -0400

    inicio de sesion persistida

--- src/components/PersistAuth.jsx
+++ src/components/PersistAuth.jsx
@@ -1,5 +1,4 @@
 // Mantener sesion persistida
-
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from '../hooks/useAuth';
 import { useEffect, useState } from "react";
@@ -7,48 +6,48 @@ import axios from "axios";
 import { Spinner } from "@chakra-ui/react";
 
 const PersistAuth = () => {
-    const location = useLocation()
-    const { auth, setAuth } = useAuth()
-    const [isLoading, setIsLoading] = useState(auth.name ? false : true)
-    // console.log('inicio sesion',auth.name)
-    // Obtener el usuario cada vez que cambia la url o se refresca la pagina para mantener la sesion persistida
-    useEffect(() => {
-        const handleUser = async () => {
-            try {
-                const { data } = await axios.get('/api/refres')
-                setAuth(data)
-                console.log(data.name)
-                setIsLoading(false)
-            } catch (error) {
-                console.log(error);
-                setIsLoading(false)
-                setAuth({})
-            }
-        }
-        handleUser()
+  const location = useLocation();
+  const { auth, setAuth } = useAuth();
+  const [isLoading, setIsLoading] = useState(auth.name ? false : true);
+  // console.log('inicio sesion',auth.name)
+  // Obtener el usuario cada vez que cambia la url o se refresca la pagina para mantener la sesion persistida
+  useEffect(() => {
+    const handleUser = async () => {
+      try {
+        const { data } = await axios.get('/api/refres');
+        setAuth(data);
+        console.log(data.name);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setAuth({});
+      }
+    };
+    handleUser();
 
-    }, [setAuth]);
+  }, [setAuth]);
 
-    //CUANDO CARGARDO EL USUARIO
-    if (isLoading) {
-        return <div><Spinner  size='md' color="red.600"/> cargandoo!</div>
-    }
+  //CUANDO CARGARDO EL USUARIO
+  if (isLoading) {
+    return <div><Spinner  size='md' color="red.600"/> cargandoo!</div>;
+  }
 
-    //cuando estoy en home
-    if (location.pathname === '/' || location.pathname === '/Home') {
-        if (auth?.name) {
-            return <Navigate to='/SidebarWithHeader' state={{ from: location }} replace />
-        } else {
-            return <Outlet />
-        }
-    }
-
-    //cuando estoy en cualquier ruta privada
+  //cuando estoy en home
+  if (location.pathname === '/') {
     if (auth?.name) {
-        return <Outlet />
+      return <Navigate to='/dashboard' state={{ from: location }} replace />;
     } else {
-        return <Navigate to='/Home' state={{ from: location }} replace />
+      return <Outlet />;
     }
+  }
+
+  //cuando estoy en cualquier ruta privada
+  if (auth?.name) {
+    return <Outlet />;
+  } else {
+    return <Navigate to='/' state={{ from: location }} replace />;
+  }
 
 
 };
