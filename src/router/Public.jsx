fc797d1c911ec6561f24d5380142592fa2c38eff
merwil vegas <com2pa@gmail.com>
2024-07-21 14:35:57 -0400

    se elimino los layout menu y footer

--- src/router/Public.jsx
+++ src/router/Public.jsx
@@ -1,11 +1,10 @@
 
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Services from '../pages/Services';
 import Contact from '../pages/Contact';
-import Footer from '../layout/Footer';
-import Menu from '../layout/Menu';
+
 import Index from '../pages/Login/Index';
 import RegisterFrom from '../pages/Login/RegisterForm';
 import LoginForm from '../pages/Login/LoginForm';
@@ -14,27 +13,24 @@ import PersistAuth from '../components/PersistAuth';
 
 
 export const Root = () => {
-    return (
-        <>
+  return (
+    <>
+      <Routes>
+        <Route element={<PersistAuth />}>
+          <Route path='/' element={<Home />} />   
+        </Route>
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/sesion' element={<Index />} />
+        <Route path='/register' element={<RegisterFrom />} />
+        <Route path='/login' element={<LoginForm />} />
+        <Route path='/verify/:id/:token' element={<Verify />} />
+      </Routes>
 
-            {/* <Menu /> */}
-            <Routes>
-                <Route element={<PersistAuth />}>
-                    <Route path='/' element={<Home />} />
-                    
-                </Route>
-                <Route path='/about' element={<About />} />
-                <Route path='/services' element={<Services />} />
-                <Route path='/contact' element={<Contact />} />
-                <Route path='/sesion' element={<Index />} />
-                <Route path='/register' element={<RegisterFrom />} />
-                <Route path='/login' element={<LoginForm />} />
-                <Route path='/verify/:id/:token' element={<Verify />} />
-            </Routes>
-            {/*   */}
-        </>
-    )
+    </>
+  );
 
-}
+};
 
 export default Root;
