b0f59665d1119dd7cdec4c9265ff92d10462c62c
merwil vegas <com2pa@gmail.com>
2024-07-21 14:22:53 -0400

    se añadio auntenticacion de usuario

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,12 +3,12 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({});
-    return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
-            {children}
-        </AuthContext.Provider>
-    )
+  const [auth, setAuth] = useState({});
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
