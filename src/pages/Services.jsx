7a54c6f50058a9962a396a2a756e062de347f0a2
merwil vegas <com2pa@gmail.com>
2024-08-09 12:34:54 -0400

    estructura final y resposive

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,108 +1,102 @@
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, Button, Container, Flex, Heading, Text } from '@chakra-ui/react';
+import {Box, Card, Flex, Heading, Image, ListItem, OrderedList, Text } from '@chakra-ui/react';
 import Menu from '../layout/Menu';
+import Enfermera from '../assets/Enfermera.jpg';
+import Footer from '../layout/Footer';
 export const Services = () => {
   return (
     <>
-      <Menu/>
-      <Box h="40vh" bg="red.600" display="flex" alignItems="center" justifyContent="center">
-        <Flex alignContent="center" justifyContent="center">
-          <Heading color="white" display="flex" mb={2} justifyContent="center" > Servicios de Enfermeria at Home</Heading>
-        </Flex>      
-      </Box>
-      <Box h="60vh" display="flex" justifyContent="center" alignItems="center">
-        <Flex
-          minH={'100vh'}
-          align={'center'}
-          justify={'center'}
-          bg="grey.200"
-        >
-          {/* <Container> */}
-          <Accordion allowMultiple width="100%" maxW="lg" rounded="lg">
-            <AccordionItem>
-              <AccordionButton
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-                p={4}>
-                <Text fontSize="md">Nuestros Servicios de Enfermería</Text>
-                <ChevronDownIcon fontSize="24px" />
-              </AccordionButton>
-              <AccordionPanel pb={4}>
-                <Text color="gray.600">
+      <Flex flexDir={"column"} gap={8} p={8} maxW="90rem" mx="auto">
+        <Menu/>   
+        <Card h="20vh" bg="red.600" display="flex" alignItems={{base:'center',md:'center'}} textAlign={{base:'center',md:'center'}} justifyContent={{base:'center',md:'center'}}>
+          <Heading color="white" fontSize={{base:'25',sm:'30',md:'35',lg:'40',xl:'45'}} > Servicios de Enfermeria at Home</Heading>
+        </Card>
+        <Flex gap={4} flexDir={{ base: 'column', md: 'row', }}>
+          <Card variant="outline" w="100%" justifyContent={{lg:'center'}} >
+            <Flex flexDir="column" p={4}  gap="3rem">
+              <Text color="red.600"   fontWeight="600" fontSize={{ base: 25, md: 30,lg:40,xl:45,}}textAlign={{base:'center', md:'center',lg:'center'}} >
+                Nuestros Servicios de Enfermería
+              </Text>
+              <Text fontSize={{ base: 15, md: 20 ,lg:25,xl:30}}textAlign={{base:'justify', md:'justify',lg:'justify',xl:'justify'}}>
               Nuestro Servicio de Enfermería a domicilio está formado por profesionales 
               capacitados para asistir pacientes en su proceso de sanación del dolor y la enfermedad.
-               
-                </Text>
-              </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem>
-              <AccordionButton
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-                p={4}>
-                <Text fontSize="md">¿En qué casos son necesarios nuestros servicios?</Text>
-                <ChevronDownIcon fontSize="24px" />
-              </AccordionButton>
-              <AccordionPanel pb={4}>
-                <Text color="gray.600">
-                  <ul>
-                    <li>Patologías agudas o crónicas que ameriten cuidados de enfermería continúo, 
-                    para cumplimiento de tratamiento endovenoso o hidratación con la supervisión 
-                    constante de signos vitales y estado general del paciente.</li>
-                    <li>Realización de curas complicadas como consecuencia de</li>
-                    <li>Cuidados paliativos en caso de pacientes terminales.</li>
-                    <li>Cuidados y apoyo post-parto.</li>
-
-                  </ul>
-               
-                </Text>
-              </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem>
-              <AccordionButton
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-                p={4}>
-                <Text fontSize="md">Igualmente prestamos los siguientes servicios:</Text>
-                <ChevronDownIcon fontSize="24px" />
-              </AccordionButton>
-              <AccordionPanel pb={4}>
-                <Text color="gray.600">
-                  <ul>
-                    <li>Toma de muestras de sangre para laboratorio.</li>
-                    <li>Toma de vías y aplicación de tratamiento endovenoso según horario indicado por médico tratante.</li>
-                  
-
-                  </ul>
-                </Text>
-              </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem>
-              <AccordionButton
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-                p={4}>
-                <Text fontSize="md">Para cada Paciente</Text>
-                <ChevronDownIcon fontSize="24px" />
-              </AccordionButton>
-              <AccordionPanel pb={4}>
-                <Text color="gray.600">
-              diseñamos un Plan de Atención de Enfermería de acuerdo a sus necesidades procurando siempre 
-              el Bienestar integral.
-              Con este fin ofrecemos una Visita de Primer Contacto que 
-              nos permitirá evaluar integralmente al paciente y su entorno, 
-              con el objeto de realizar un diagnóstico exhaustivo y planificar el servicio requerido.
-                </Text>
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
-          {/* </Container> */}
+              </Text> 
+            </Flex>
+          </Card>
+          <Card variant="outline" width="100%" height="30rem" display={{base: 'none', md: 'block'}}>
+            <Image src={Enfermera} alt="cerrar-equipo-listo-trabajar" height="100%" objectFit="cover" w="100%"  />
+          </Card>
         </Flex>
-      </Box>
+        <Box>
+          <Flex gap={4} flexDir={{ base: 'column', md: 'row', }} justifyContent={{lg:'center'}}>
+            <Card variant="outline" w="100%" >
+              <Flex flexDir="column" p={4}  gap="3rem">
+                <Text color="red.600" fontSize={{ base: 25, md: 30,lg:40,xl:45}}textAlign={{base:'center', md:'center',lg:'center',xl:45}} fontWeight="600">
+                  ¿En qué casos son necesarios nuestros servicios?
+                </Text>
+                <Text fontSize={{ base: 15, md: 20 ,lg:25,xl:30}} textAlign={{base:'justify', md:'justify',lg:'justify',xl:'justify'}}>                
+                  <OrderedList>
+                    <ListItem>Patologías agudas o crónicas que ameriten cuidados de enfermería continúo, 
+                    para cumplimiento de tratamiento endovenoso o hidratación con la supervisión 
+                    constante de signos vitales y estado general del paciente.</ListItem>
+                    <ListItem>Realización de curas complicadas como consecuencia de</ListItem>
+                    <ListItem>Cuidados paliativos en caso de pacientes terminales.</ListItem>
+                    <ListItem>Cuidados y apoyo post-parto.</ListItem>
+
+                  </OrderedList>
+               
+                
+                </Text> 
+              </Flex>
+            </Card>
+            <Card variant="outline" width="100%" height="auto" display={{base: 'none', md: 'block',lg:'block'}}>
+              <Image src={Enfermera} alt="cerrar-equipo-listo-trabajar" height="100%" objectFit="cover" w="100%"  />
+            </Card>          
+          </Flex>
+        </Box>
+        <Box>
+          <Flex gap={4} flexDir={{ base: 'column', md: 'row', }} >
+            <Card variant="outline" w="100%" justifyContent={{lg:'center'}}>
+              <Flex flexDir="column" p={4}  gap="3rem">
+                <Text color="red.600" fontSize={{ base: 25, md: 30,lg:40,xl:45}}textAlign={{base:'center', md:'center',lg:'center',xl:'center'}} fontWeight="600">
+                  Igualmente prestamos los siguientes servicios:
+                </Text>
+                <Text fontSize={{ base: 15, md: 20 ,lg:25,xl:30}} textAlign={{base:'justify', md:'justify',lg:'justify',xl:'justify'}}>                
+                  <OrderedList>
+                    <ListItem>Toma de muestras de sangre para laboratorio.</ListItem>
+                    <ListItem>Toma de vías y aplicación de tratamiento endovenoso según horario indicado por médico tratante.</ListItem>
+                  </OrderedList>  
+                </Text> 
+              </Flex>
+            </Card>
+            <Card variant="outline" width="100%" height="30rem" display={{base: 'none', md: 'block'}}>
+              <Image src={Enfermera} alt="cerrar-equipo-listo-trabajar" height="100%" objectFit="cover" w="100%"  />
+            </Card>          
+          </Flex>
+        </Box>
+        <Box>
+          <Flex gap={4} flexDir={{ base: 'column', md: 'row', }}>
+            <Card variant="outline" w="100%"  justifyContent={{lg:'center'}}>
+              <Flex flexDir="column" p={4}  gap="3rem">
+                <Text color="red.600" fontSize={{ base: 25, md: 30,lg:40,xl:45}}textAlign={{base:'center', md:'center',lg:'center',xl:'center'}} fontWeight="600">
+                  Para cada Paciente
+                </Text>
+                <Text fontSize={{ base: 15, md: 20 ,lg:25,xl:'30'}} textAlign={{base:'justify', md:'justify',lg:'justify',xl:'justify'}}>                
+                Diseñamos un Plan de Atención de Enfermería de acuerdo a sus necesidades procurando siempre 
+                el Bienestar integral.
+                Con este fin ofrecemos una Visita de Primer Contacto que 
+                nos permitirá evaluar integralmente al paciente y su entorno, 
+                con el objeto de realizar un diagnóstico exhaustivo y planificar el servicio requerido.
+                </Text> 
+              </Flex>
+            </Card>
+            <Card variant="outline" width="100%" height="30rem" display={{base: 'none', md: 'block'}}>
+              <Image src={Enfermera} alt="cerrar-equipo-listo-trabajar" height="100%" objectFit="cover" w="100%"  />
+            </Card>          
+          </Flex>
+        </Box>
+        <Footer/>
+
+      </Flex>
     </>
 
 
